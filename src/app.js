c9f192e8170d752eb9f6ca87c7e88c46643ee299
dharmikpandav1311 <dhamopandav1311@gmail.com>
2021-02-01 15:29:31 +0530

    update

--- src/app.js
+++ src/app.js
@@ -53,7 +53,7 @@ app.get('/help/*', (req, res) => {
 app.get('/weather', (req, res) =>{
     if(!req.query.address){
         return res.send({
-            error: 'You must provide adreess'
+            error: 'You must provide adreess.'
         });
     }
  /// Adress :: Surat,Gujarat India
